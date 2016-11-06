812c72b218a17bf035c2ed46a2051fb5cd282844
diligentyang <905407339@qq.com>
2016-11-06 11:41:16 +0800

    第二章

--- 第2章 写的都是看到的——面向对象编程/test.js
+++ 第2章 写的都是看到的——面向对象编程/test.js
@@ -1,1 +1,20 @@
-﻿
+﻿var Book = function(id, name, price){
+    //私有属性
+    var num = 1;
+    //私有方法
+    function checkId(){
+
+    };
+    //特权方法
+    this.getName =  function(){};
+    this.getPrice = function(){};
+    this.setName = function(){};
+    this.setPrice = function(){};
+    //对象的公有属性
+    this.id = id;
+    //对象的公有方法
+    this.copy = function(){};
+    //构造器
+    this.setName(name);
+    this.setPrice(price);
+}
