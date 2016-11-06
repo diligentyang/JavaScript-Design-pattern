8c22127cb9213643f7a752267bab55349f3a26e7
diligentyang <905407339@qq.com>
2016-11-06 15:31:11 +0800

    原型时继承

--- 第2章 写的都是看到的——面向对象编程/test.js
+++ 第2章 写的都是看到的——面向对象编程/test.js
@@ -201,6 +201,7 @@ instance1.showBooks();　　　　　　// TypeError
 
 // 组合式继承
 // 声明父类
+/**
 function SuperClass(name){
     // 值类型共有属性
     this.name = name;
@@ -235,3 +236,31 @@ console.log(instance2.books);　　　// ["html", "css", "JavaScript"]
 instance2.getName();　　　　　　　// css book
 instance2.getTime();　　 　　　　// 2013
 
+**/
+
+// 原型是继承
+function inheritObject(o){
+    // 声明一个过渡函数对象
+    function F(){}
+    // 过渡对象的原型继承父对象
+    F.prototype = o;
+    // 返回过渡对象的一个实例，该实例的原型继承了父对象
+    return new F();
+}
+
+var book = {
+    name: "js book",
+    alikeBook: ["css book", "html book"]
+};
+var newBook = inheritObject(book);
+newBook.name = "ajax book";
+newBook.alikeBook.push("xml book");
+var otherBook = inheritObject(book);
+otherBook.name = "flash book";
+otherBook.alikeBook.push("as book");
+console.log(newBook.name);　　　　　　//ajax book
+console.log(newBook.alikeBook);　　　　//["css book", "html book", "xml book", "as book"]
+console.log(otherBook.name);　　　　//flash book
+console.log(otherBook.alikeBook);　　//["css book", "html book", "xml book", "as book"]
+console.log(book.name); 　　　　　　//js book
+console.log(book.alikeBook); 　　　　//["css book", "html book", "xml book", "as book"]
