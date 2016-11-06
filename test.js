a338d55528c6ce3aed2df2e6b9292c396416536d
diligentyang <905407339@qq.com>
2016-11-06 11:21:47 +0800

    灵活的语言JavaScript代码

---
+++ test.js
@@ -0,0 +1,83 @@
+﻿/*var obj = {
+	checkName : function (){
+		alert("checkName");
+	},
+	checkEmail : function (){
+		alert("checkEmail");
+	},
+	checkPassword : function (){
+		alert("checkPassword");
+	}
+}
+
+obj.checkName();*/
+
+/*var obj = function (){
+	return {
+		checkName : function (){
+			alert("checkName");
+		},
+		checkEmail : function (){
+			alert("checkEmail");
+		},
+		checkPassword : function (){
+			alert("checkPassword");
+		}
+	}
+}
+
+var a = obj();
+a.checkName();*/
+
+/*var obj = function (){
+	this.checkName = function () {
+		alert("checkName");
+	}
+	this.checkEmail = function () {
+		alert("checkEmail");
+	}
+	this.checkPassword = function () {
+		alert("checkPassword");
+	}
+}
+
+var a = new obj();
+a.checkName();
+*/
+
+/*var obj = function () {};
+obj.prototype.checkName = function () {
+	alert("checkName");
+}
+obj.prototype.checkEmail = function () {
+	alert("checkEmail");
+}
+obj.prototype.checkPassword = function () {
+	alert("checkPassword");
+}
+
+var a = new obj();
+a.checkName();*/
+
+
+/*Function.prototype.checkName = function () {
+	alert("checkName");
+}
+
+var a = function(){};
+a.checkName();*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
