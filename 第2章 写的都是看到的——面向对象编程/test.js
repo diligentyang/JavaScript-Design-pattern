c6c1b96394cc0385823dc789b2b174f08d9fb9d6
diligentyang <905407339@qq.com>
2016-11-06 15:40:02 +0800

    寄生式继承

--- 第2章 写的都是看到的——面向对象编程/test.js
+++ 第2章 写的都是看到的——面向对象编程/test.js
@@ -239,6 +239,7 @@ instance2.getTime();　　 　　　　// 2013
 **/
 
 // 原型是继承
+/**
 function inheritObject(o){
     // 声明一个过渡函数对象
     function F(){}
@@ -264,3 +265,55 @@ console.log(otherBook.name);　　　　//flash book
 console.log(otherBook.alikeBook);　　//["css book", "html book", "xml book", "as book"]
 console.log(book.name); 　　　　　　//js book
 console.log(book.alikeBook); 　　　　//["css book", "html book", "xml book", "as book"]
+
+ **/
+
+
+function inheritObject(o){
+    // 声明一个过渡函数对象
+    function F(){}
+    // 过渡对象的原型继承父对象
+    F.prototype = o;
+    // 返回过渡对象的一个实例，该实例的原型继承了父对象
+    return new F();
+}
+
+/**
+ * 寄生式继承 继承原型
+ * 传递参数 subClass　 子类
+ * 传递参数 superClass 父类
+ **/
+function inheritPrototype(subClass, superClass){
+    // 复制一份父类的原型副本保存在变量中
+    var p = inheritObject(superClass.prototype);
+    // 修正因为重写子类原型导致子类的constructor属性被修改
+    p.constructor = subClass;
+    // 设置子类的原型　　　　　
+    subClass.prototype = p;
+}
+
+// 定义父类　　　　　　　　　　
+function SuperClass(name){
+    this.name = name;
+    this.colors = ["red", "blue", "green"];
+}
+// 定义父类原型方法
+SuperClass.prototype.getName = function(){
+    console.log(this.name);
+};
+// 定义子类
+function SubClass(name, time){
+    // 构造函数式继承
+    SuperClass.call(this, name);
+    // 子类新增属性
+    this.time = time;
+}
+// 寄生式继承父类原型
+inheritPrototype(SubClass, SuperClass);
+// 子类新增原型方法
+SubClass.prototype.getTime = function(){
+    console.log(this.time);
+};
+// 创建两个测试方法
+var instance1 = new SubClass("js book", 2014);
+var instance2 = new SubClass("css book", 2013);
