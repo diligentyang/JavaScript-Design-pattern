9ca4037cb1c7d461d37e5fbe3c13d2c484b815de
diligentyang <905407339@qq.com>
2016-11-06 15:06:11 +0800

    构造函数式继承

--- 第2章 写的都是看到的——面向对象编程/test.js
+++ 第2章 写的都是看到的——面向对象编程/test.js
@@ -156,6 +156,7 @@ console.log(window.time);　 　　// undefined
 console.log(window.type);　 　　// undefined
 **/
 
+/**
 function SuperClass(){
     this.books = ['JavaScript', 'html', 'css'];
 }
@@ -166,10 +167,35 @@ var instance2 = new SubClass();
 console.log(instance2.books);　　// ["JavaScript", "html", "css"]
 instance1.books.push('设计模式');
 console.log(instance2.books);　　// ["JavaScript", "html", "css", "设计模式"]
+**/
 
-
-
-
+//构造函数式继承
+// 声明父类
+function SuperClass(id){
+    // 引用类型共有属性
+    this.books = ['JavaScript', 'html', 'css'];
+    // 值类型共有属性
+    this.id = id;
+}
+// 父类声明原型方法
+SuperClass.prototype.showBooks = function(){
+    console.log(this.books);
+}
+// 声明子类
+function SubClass(id){
+    // 继承父类
+    SuperClass.call(this, id);
+}
+// 创建第一个子类的实例
+var instance1 = new SubClass(10);
+// 创建第二个子类的实例
+var instance2 = new SubClass(11);
+instance1.books.push("设计模式");
+console.log(instance1.books);　 　　// ["JavaScript", "html", "css", "设计模式"]　
+console.log(instance1.id);　　　　// 10
+console.log(instance2.books); 　　// ["JavaScript", "html", "css"]
+console.log(instance2.id); 　　　　// 11
+instance1.showBooks();　　　　　　// TypeError
 
 
 
